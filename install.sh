15f59d39ac9d6e51c68e1c7bf12b8360e9c20333
Rajath S <rajaths.rajaths@gmail.com>
2015-09-24 18:41:13 +0530

    Update install.sh - to create directory strcuture

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 echo "Installing Python Modules"
 
 python setup_python.py
+mkdir include
+mkdir prog_data
+mkdir prog_data/persistent
+mkdir prog_data/seek_data
+mkdir temp
+mkdir temp/aux_index
+mkdir temp/aux_index/DOCPATH
+mkdir temp/aux_index/WPATH
+mkdir data
+mkdir data/html
+mkdir data/txt
+sh shell_del.sh
 
 echo "Installing STXXL"
 cd include
